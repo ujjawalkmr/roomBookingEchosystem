ec8b911e41104fca756de088016faa933536950c
Ujjawal Kumar <ujjawalkmr9211@gmail.com>
2026-06-28 23:03:14 +0530

    role add in user model
    Signed-off-by: Ujjawal Kumar <ujjawalkmr9211@gmail.com>

--- services/authService/src/models/user.model.js
+++ services/authService/src/models/user.model.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin", "moderator"],
+      default: "user",
+    },
     refreshToken: String,
   },
   {
